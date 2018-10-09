6da251a535fab7e3a57b6669d76121019c3c532b
slava <vzelinskiy@stud.onu.edu.ua>
2018-10-09 15:25:56 +0300

    showme1

--- Transpost.php
+++ Transpost.php
@@ -4,9 +4,15 @@ abstract class Transport{
 	public $color;
 	public $people_amount;
 	public $speed;
+	public $name;
 	function __construct($price = 0, $color = "black", $people_amount = 0, $speed = 0){
 		$this->price = $price;
 		$this->color = $color;
+	}
+	public function showme(){
+		echo "<hr>";
+		echo "My name is: {$this->name}. I cost: {$this->price}";
+		echo "</hr>";
 	}
 }
 ?>
